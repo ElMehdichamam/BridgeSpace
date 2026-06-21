258c6d06520a33172286ff09387c87f0876b57df
mehdichamam <mehdichamam465@gmail.com>
2026-06-21 18:04:38 +0000

    fixing the middlware

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -14,8 +14,8 @@ const verifyToken = async (req,res,next) =>{
 
     try {
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        const user = User.findById(decoded.id).select("-password");
-        req.user=decoded;
+        const user = await User.findById(decoded.id).select("-password");
+        req.user = user;
         next()
     } catch (err) {
         return res.status(401).json({
