6b2256bc12e66bf357f4a4b7b35e72c839ed2d9e
mehdichamam <mehdichamam465@gmail.com>
2026-06-21 18:02:12 +0000

    Passing The  middlware to The Me Router

--- Backend/router/auth.routes.js
+++ Backend/router/auth.routes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const {loginController,registerController,getMe}= require("../controllers/auth.controller");
 
+const verifyToken = require("../middleware/auth.middleware");
+
 router.post('/register',registerController);
 
 router.post('/login',loginController);
 
-router.get('/me',getMe);
+router.get('/me',verifyToken,getMe);
 
 module.exports = router;
