684d18084f41b3b002ccb5665a08639cd2680c04
mehdichamam <mehdichamam465@gmail.com>
2026-06-21 18:01:21 +0000

    Adding A middlware for Me Router

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+const verifyToken = async (req,res,next) =>{
+
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({
+            message:"Invalid Token"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        const user = User.findById(decoded.id).select("-password");
+        req.user=decoded;
+        next()
+    } catch (err) {
+        return res.status(401).json({
+            error:err
+        });
+    }
+}
+module.exports = verifyToken;
