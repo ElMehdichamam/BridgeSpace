f53aa3cb01f80afc94d662a2ff40fedf5f20edcd
mehdichamam <mehdichamam465@gmail.com>
2026-06-24 14:55:52 +0000

    modifying on validator admin and sender come from user itself so it's not mandatory to send them in the request otherwise nothing gonna work

--- Backend/validators/message.validator.js
+++ Backend/validators/message.validator.js
@@ -2,7 +2,7 @@ const z = require("zod");
 
 const messageValid = z.object({
     content : z.string().trim().min(1),
-    sender : z.string().trim().length(24),
+    sender : z.string().trim().length(24).optional(),
     thread : z.string().trim().length(24)
 });
 
--- Backend/validators/project.validator.js
+++ Backend/validators/project.validator.js
@@ -4,7 +4,7 @@ const projectSchema = z.object({
     name : z.string().trim().min(5),
     description : z.string().trim(),
     organization : z.string().trim().length(24),
-    admin : z.string().trim().length(24),
+    admin : z.string().trim().length(24).optional(),
     member : z.array(z.string().length(24)).optional(),
     status : z.enum(["active", "on-hold", "completed"]),
     deadline : z.string()
